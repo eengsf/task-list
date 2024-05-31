47eef56dff04072fb900a87bc36388c547ec6e15
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-05-31 20:26:58 +0700

    commit pertama

---
+++ app/editTask/[id]/page.jsx
@@ -0,0 +1,26 @@
+import EditTaskForm from "@/components/EditTaskForm";
+
+const getTaskById = async (id) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch task");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditTask({ params }) {
+  const { id } = params;
+  const { task } = await getTaskById(id);
+  const { title, description } = task;
+  return (
+    <>
+      <EditTaskForm id={id} title={title} description={description} />
+    </>
+  );
+}
